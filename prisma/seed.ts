bf979b7860292ea77138cc7862a28126dd275d82
jenebm <jenebmercado@gmail.com>
2025-12-13 00:01:38 -1000

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -28,7 +28,7 @@ async function main() {
             profileImage: user.profileImage,
             followers: user.followers,
             following: user.following,
-            gameInterests: user.gameInterests || [],
+            gameInterestIds: user.gameInterestIds || [],
             gameTags: user.gameTags || [],
             createdAt: new Date(user.createdAt),
             updatedAt: new Date(user.updatedAt),
